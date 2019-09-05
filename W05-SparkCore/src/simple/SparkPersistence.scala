ce2e86d5b44b34857045d0643e2d565d43e08802
suriarasai <suria.r.asai@gmail.com>
2019-09-04 18:55:35 -0700

    sys files deleted

--- W05-SparkCore/src/simple/SparkPersistence.scala
+++ W05-SparkCore/src/simple/SparkPersistence.scala
@@ -8,32 +8,34 @@ import org.apache.log4j.Level
 import org.apache.log4j.Logger
 
 object SparkPersistence {
-    Logger.getLogger("org").setLevel(Level.OFF)
-   
-  
-    def main(args:Array[String]): Unit = {
 
-  val conf = new SparkConf().setAppName("Persisting RDD").setMaster("local[2]")
-     val sc = new SparkContext(conf)
+  def main(args: Array[String]): Unit = {
+    Logger.getLogger("org").setLevel(Level.INFO)
+
+    val conf = new SparkConf().setAppName("Persisting RDD").setMaster("local[2]")
+    val sc = new SparkContext(conf)
 
     val inputRdd =
-      sc.parallelize(Array("this,is,a,ball","it,is,a,cat","Pragathi,is,in,the,temple "))
+      sc.parallelize(Array("this,is,a,ball", "it,is,a,cat", "Pragathi,is,in,the,temple "))
     val wordsRdd = inputRdd.flatMap(record => record.split(","))
-    val wordLengthPairs = wordsRdd.map(word=> (word, word.length))
-    val wordPairs = wordsRdd.map(word => (word,1))
-    val reducedWordCountRdd = wordPairs.reduceByKey((x,y) => x+y)
-    val filteredWordLengthPairs = wordLengthPairs.filter{case(word,length) =>
-      length >=3}
+    val wordLengthPairs = wordsRdd.map(word => (word, word.length))
+    val wordPairs = wordsRdd.map(word => (word, 1))
+    val reducedWordCountRdd = wordPairs.reduceByKey((x, y) => x + y)
+    val filteredWordLengthPairs = wordLengthPairs.filter {
+      case (word, length) =>
+        length >= 3
+    }
     reducedWordCountRdd.cache()
     val joinedRdd = reducedWordCountRdd.join(filteredWordLengthPairs)
     joinedRdd.persist(StorageLevel.MEMORY_AND_DISK)
     val wordPairsCount = reducedWordCountRdd.count
     val wordPairsCollection = reducedWordCountRdd.take(10)
     val joinedRddCount = joinedRdd.count
+    println("Total join data count: %s".format(joinedRddCount.doubleValue()))
     val joinedPairs = joinedRdd.collect()
     reducedWordCountRdd.unpersist()
     joinedRdd.unpersist()
-    
+
   }
-    
+
 }
