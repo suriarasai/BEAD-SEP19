e90931c335397785ea24dd1c6e4b9fb17a65013d
suria.r.asai <suria.r.asai@gmail.com>
2019-09-09 20:18:12 -0700

    refactored for small case letters

--- W07-SparkML/src/basic/MovieData.scala
+++ W07-SparkML/src/basic/MovieData.scala
@@ -2,7 +2,7 @@ package basic
 
 object MovieData {
 
-    def getMovieYearsCountSorted(): scala.Array[(Int,String)] = {
+  def getMovieYearsCountSorted(): scala.Array[(Int, String)] = {
     val movie_data_df = Util.getMovieDataDF()
     movie_data_df.createOrReplaceTempView("movie_data")
     movie_data_df.printSchema()
@@ -22,12 +22,10 @@ object MovieData {
   def main(args: Array[String]) {
 
     val movie_years = MovieData.getMovieYearsCountSorted()
-    for( a <- 0 to (movie_years.length -1)){
+    for (a <- 0 to (movie_years.length - 1)) {
       println(movie_years(a))
     }
 
   }
-
-
 
 }
